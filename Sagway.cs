b17331e7df79460625ff6b5d7233d5514e57a61a
rons1 <ron.shmuter@gmail.com>
2019-07-14 14:16:42 +0300

    Finished 1st exercise

---
+++ Sagway.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Sagway : Vehicle
+    {
+        protected string madeIn;
+
+        public Sagway(string model, float cylinder, string color, int numberOfWheels, string madeIn) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.madeIn = madeIn;
+        }
+
+        public override string ToString()
+        {
+            return "Sagway " + base.ToString() + $" {madeIn}";
+        }
+    }
+}
