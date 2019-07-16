96f1f4fe89e49c32d4c9d0ac584e3bc7c20493fe
rons1 <ron.shmuter@gmail.com>
2019-07-16 08:59:01 +0300

    Add files via upload

---
+++ 6 - Prototype/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Circle c = new Circle(new Point(5, 5), 50.5);
+
+            Console.WriteLine(c);
+
+            DoStuffWithCircle(c.clone());
+
+            Console.WriteLine(c);
+
+            DoStuffWithCircle(c);
+
+            Console.WriteLine(c);
+        }
+
+        public static void DoStuffWithCircle(Shape c)
+        {
+            ((Circle)c).Center.X = 15;
+            ((Circle)c).Area = -5;
+        }
+    }
+}
---
+++ 6 - Prototype/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoType
+{
+    public class Rectangle : Shape
+    {
+
+        public Point TopLeftPoint { get; set; }
+
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(Point topLeftPoint, double height, double width)
+        {
+            this.TopLeftPoint = topLeftPoint;
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public override string ToString()
+        {
+            return $"Rectangle Top Left Point is {TopLeftPoint} , Height is {Height}, and Width is {Width}";
+        }
+
+        public override Shape clone()
+        {
+            return new Rectangle(TopLeftPoint.clone(), Height, Width);
+        }
+    }
+}
